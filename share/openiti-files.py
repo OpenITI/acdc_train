7f2fa1e8c6321dde8fb33c88c557e03807b98ac4
David Smith <dasmiq@gmail.com>
2024-06-15 09:21:20 -0400

    Refactor cleanOpenITI to a function.

--- share/openiti-files.py
+++ share/openiti-files.py
@@ -1,6 +1,19 @@
 import argparse, os
+import regex
+from regex import sub
 from pyspark.sql import SparkSession, Row
-from pyspark.sql.functions import col, regexp_replace, translate
+from pyspark.sql.functions import col, regexp_replace, translate, udf
+
+def cleanOpenITI(text):
+    text = sub(r'^#META.*\n*', '', text, flags=regex.M)
+    text = sub(r'#+OpenITI#\n*', '', text)
+    text = sub(r'[ ]+\n', '\n',
+               sub(r'\n[ ]+', '\n',
+                   sub(r' [ ]+', ' ',
+                       sub(r'\p{P}', ' ',
+                           sub(r'PageV\S+|ms\d+|\p{M}', '',
+                               text)))))
+    return text
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Import OpenITI',
@@ -14,19 +27,12 @@ if __name__ == '__main__':
     wd = os.getcwd()
     wd = wd if wd.startswith('file:') else 'file:' + wd
 
+    clean_openiti = udf(lambda text: cleanOpenITI(text))
+
     spark.read.load(config.inputPath, format='binaryFile', recursiveFileLookup='true'
+        ).filter(~col('path').endswith('.yml') & ~col('path').endswith('.md')
         ).select(regexp_replace('path', '^' + wd + '/', '').alias('id'),
-                 regexp_replace(
-                     regexp_replace(
-                         regexp_replace(
-                             regexp_replace(
-                                 regexp_replace(translate(col('content').cast('string'),
-                                                          '#~%|', ''),
-                                                r'PageV\S+|ms\d+|\p{M}', ''),
-                                 r'\p{P}', ' '),
-                             r' [ ]+', ' '),
-                         r'\n[ ]+', '\n'),
-                     r'[ ]+\n', '\n').alias('text')
+                 clean_openiti(col('content').cast('string')).alias('text')
         ).write.json(config.outputPath, mode='overwrite')
 
     spark.stop()
